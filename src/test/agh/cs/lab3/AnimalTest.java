90a2eb68caf85650e2b4f5d5d27848f2644aba51
Mateusz Łączny <mateusz.laczny@interia.pl>
2020-11-05 12:45:29 +0100

    Zadania z labolatorium 4. Zmodyfikowano testy klasy Animal by odpowiadały one nowej implementacji klasy

--- src/test/agh/cs/lab3/AnimalTest.java
+++ src/test/agh/cs/lab3/AnimalTest.java
@@ -3,6 +3,7 @@ package agh.cs.lab3;
 import agh.cs.lab2.MapDirection;
 import agh.cs.lab2.MoveDirection;
 import agh.cs.lab2.Vector2d;
+import agh.cs.lab4.RectangularMap;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,31 +11,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class AnimalTest {
     @Test
     void correctlyCreatesObject() {
-        Animal testAnimal = new Animal();
+        RectangularMap map = new RectangularMap(4, 4);
+        Animal testAnimal = new Animal(map);
         assertEquals(MapDirection.NORTH, testAnimal.getOrientation());
         assertEquals(new Vector2d(2, 2), testAnimal.getPosition());
     }
 
     @Test
     void testToString() {
-        Animal testAnimal = new Animal();
-        assertEquals("orientation=Północ, position=(2, 2)", testAnimal.toString());
+        RectangularMap map = new RectangularMap(4, 4);
+        Animal testAnimal = new Animal(map);
+        assertEquals("^", testAnimal.toString());
     }
 
     @Test
     void movesInGivenDirection() {
-        Animal testAnimal1 = new Animal();
+        RectangularMap map = new RectangularMap(4, 4);
+        Animal testAnimal1 = new Animal(map);
         testAnimal1.move(MoveDirection.FORWARD);
         assertEquals(new Vector2d(2, 3), testAnimal1.getPosition());
 
-        Animal testAnimal2 = new Animal();
+        Animal testAnimal2 = new Animal(map);
         testAnimal2.move(MoveDirection.BACKWARD);
         assertEquals(new Vector2d(2, 1), testAnimal2.getPosition());
     }
 
     @Test
     void correctlyChangesOrientationClockwise() {
-        Animal testAnimal1 = new Animal();
+        RectangularMap map = new RectangularMap(4, 4);
+
+        Animal testAnimal1 = new Animal(map);
         testAnimal1.move(MoveDirection.RIGHT);
         assertEquals(MapDirection.EAST, testAnimal1.getOrientation());
 
@@ -50,7 +56,9 @@ class AnimalTest {
 
     @Test
     void correctlyChangesOrientationCounterclockwise() {
-        Animal testAnimal1 = new Animal();
+        RectangularMap map = new RectangularMap(4, 4);
+
+        Animal testAnimal1 = new Animal(map);
         testAnimal1.move(MoveDirection.LEFT);
         assertEquals(MapDirection.WEST, testAnimal1.getOrientation());
 
@@ -63,4 +71,5 @@ class AnimalTest {
         testAnimal1.move(MoveDirection.LEFT);
         assertEquals(MapDirection.NORTH, testAnimal1.getOrientation());
     }
+
 }
